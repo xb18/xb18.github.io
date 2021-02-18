101ee3807ba6516b22bdecfba3d6bfd8b4383bcb
xb18 <450010494@qq.com>
2021-02-18 21:23:36 +0800

    new post

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -12,5 +12,17 @@ module.exports = {
     '@vuepress/back-to-top',
     '@vuepress/medium-zoom',
     '@vuepress/nprogress'
-  ]
+  ],
+  head: [ ['script', {}, `
+  var _hmt = _hmt || []; 
+  (function() { 
+    var hm = document.createElement("script"); 
+    hm.src = "https://unpkg.com/github-corners/dist/embed.min.js"; 
+    hm.setAttribute('data-href','https://github.com/xb18');
+    hm.setAttribute('data-target','_blank');
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s); 
+  })(); 
+`]]
+
 }
