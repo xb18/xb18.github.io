873bbd55b8e53c13c65724f6fe06073c97754293
xb18 <450010494@qq.com>
2021-02-18 20:41:44 +0800

    new post

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  title: 'Hello VuePress',
+  title: 'xb18’ blog',
   theme: '@vuepress/blog',
   themeConfig: {
     // 请参考文档来查看所有可用的选项。
-  }
+    smoothScroll: true,
+    sidebar: 'auto',
+    lastUpdated: 'Last Updated',
+  },
+  plugins: 
+  [
+    '@vuepress/back-to-top',
+    '@vuepress/medium-zoom',
+    '@vuepress/nprogress'
+  ]
 }
