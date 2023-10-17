63ad1bc626f19ef9955816d8ae5ac3f5a4eb7a85
xb18 <450010494@qq.com>
2023-10-17 11:54:47 +0800

    Site updated: 2023-10-17 11:54:43

--- js/copy-codeblock.js
+++ js/copy-codeblock.js
@@ -1,13 +1,24 @@
 window.addEventListener('load', () => {
   const codeBlocks = document.querySelectorAll('figure.highlight');
   if (!codeBlocks.length) return;
-
   const addCopyButton = (codeBlock) => {
+    // lang
+    let lang = codeBlock.querySelector('td.code > pre > code').classList;
+    lang = Array.from(lang);
+    lang = lang.find(item => item !== 'hljs');
+    lang = lang.trim();
+    if (lang) {
+      lang = lang.toLowerCase();
+      const langElem = document.createElement('div');
+      langElem.setAttribute('class', 'codeblock-lang-wrapper');
+      langElem.setAttribute('data-lang', lang);
+      langElem.innerText = lang;
+      codeBlock.appendChild(langElem);
+    }
+    // copy
     const copyWrapper = document.createElement('div');
     copyWrapper.setAttribute('class', 'codeblock-copy-wrapper');
-
     let copiedTimeout = null;
-
     copyWrapper.addEventListener('click', (ev) => {
       const highlightDom = ev.target.parentElement;
       const code = highlightDom.querySelector('code') || highlightDom.querySelector('.code');
